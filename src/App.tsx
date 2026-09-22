da6353db52abdffb1373efce983824bece3458d6
Elina Razina <e.razina@samolet.ru>
2026-09-22 14:33:42 +0700

    added react router

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router";
+import "./App.css";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
